01ef83cbabbc3df8277406adc2a8c112507cc64d
Eduardo <eduardof@fmb.unesp.br>
2024-02-23 21:11:19 -0300

    save 401

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -53,11 +53,11 @@ export class UserController {
 	async login(req: Request, res: Response) {
 		const { email, password } = req.body
 
-		const user = await prisma.user.findFirst({
-			where: {
-			  email: email,
-			},
-		  });
+//		const user = await prisma.user.findFirst({
+//			where: {
+//			  email: email,
+//			},
+//		  });
 
 //		const token = jwt.sign({ id: user?.id }, process.env.JWT_PASS ?? '', {
 //			expiresIn: '8h',
@@ -65,8 +65,8 @@ export class UserController {
 
 
 		return res.json({
-			user: user,
-//			token: token,
+			email: email,
+			pass: password,
 		})
 	}
 
