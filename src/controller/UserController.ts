cf2c6bff15b731a2cacffe0d1aef67c2795c29b3
Eduardo <eduardof@fmb.unesp.br>
2024-02-24 22:03:28 -0300

    commit 506

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -23,8 +23,16 @@ export class UserController {
 			return res.status(400).json({ message: 'Senha é obrigatória' })
 		}
 
+		const userExists = await prisma.user.findFirst({where: {email: email}});
+
+		if (userExists) {
+			return res.status(400).json({ message: 'Email já cadastrado' })
+		}
+
+		const hashPassword = await bcrypt.hash(password, 10)
+
 		try {
-			const user = await prisma.user.create({
+			const newUser = await prisma.user.create({
 				data: {
 				  name,
 				  email,
@@ -32,6 +40,7 @@ export class UserController {
 				  createdAt: new Date(),
 				},
 			  });
+			  const { password: _, ...user } = newUser
 			  res.json(user);
 		} catch (error) {
 			return res.status(500).json({ message: 'Internal Server Error' })
@@ -40,10 +49,7 @@ export class UserController {
 
 	async list(req: Request, res: Response) {
 		try {
-			const user = await prisma.user.findMany({
-				orderBy: { createdAt: "desc" },
-			});
-			
+			const user = await prisma.user.findMany({ orderBy: { createdAt: "desc" }});
 			res.json(user);
 		} catch (error) {
 			return res.status(500).json({ message: 'Internal Sever Error' })
@@ -53,20 +59,27 @@ export class UserController {
 	async login(req: Request, res: Response) {
 		const { email, password } = req.body
 
-		const user = await prisma.user.findFirst({
-			where: {
-			  	email: email,
-			},
-		});
+		const user = await prisma.user.findFirst({ where: { email: email }});
+
+		if (!user) {
+			return res.status(400).json({ message: 'E-mail ou senha inválidos' })
+		}
+
+		const verifyPass = await bcrypt.compare(password, user.password)
+
+		if (!verifyPass) {
+			return res.status(400).json({ message: 'E-mail ou senha inválidos' })
+		}
 
 		const token = jwt.sign({ id: user?.id }, process.env.JWT_PASS ?? '', {
 			expiresIn: '8h',
 		})
 
+		const { password: _, ...userLogin } = user
 
 		return res.json({
-			user: user,
-			token: token
+			user: userLogin,
+			token: token,
 		})
 	}
 
