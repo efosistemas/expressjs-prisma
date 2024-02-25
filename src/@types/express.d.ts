c2d6bec0b52f8f41a75b1a2f4028a3e7d56bb66d
Eduardo <eduardof@fmb.unesp.br>
2024-02-25 13:39:18 -0300

    commit 520

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,8 +1,5 @@
-	declare namespace Express {
-		export interface Request {
-			user: any;
-		}
-		export interface Response {
-			user: any;
-		}
+declare namespace Express {
+	export interface Request {
+		user: any;
 	}
+}
