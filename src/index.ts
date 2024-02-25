00a0cbc88d4aa937c257d23a4cf85edc500fc9fd
Eduardo <eduardof@fmb.unesp.br>
2024-02-24 21:12:57 -0300

    commit 502

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,14 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/login", async (req, res) => {
-    res.send(`<h1>REST API Mensagens</h1>`.trim());
-});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body		
+  res.send(res.json({
+    email: email,
+    pass: password,
+  })
+)}
+)
   
 app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
