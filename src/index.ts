b6169a70fec84ce63c112b562843a41a6b9b5bdf
Eduardo <eduardof@fmb.unesp.br>
2024-02-24 21:21:20 -0300

    commit 503

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,7 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body		
-  res.send(res.json({
-    email: email,
-    pass: password,
-  })
-)}
-)
+app.use(routes)
   
 app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
